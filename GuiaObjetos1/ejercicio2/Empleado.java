a03eeaeba1637eb5ed93c2f2fe07b60d6b00b840
nicolas <nikolastesone@gmail.com>
2021-03-19 22:56:45 -0300

    Guia 2 Objetos

---
+++ GuiaObjetos1/ejercicio2/Empleado.java
@@ -0,0 +1,63 @@
+package GuiaObjetos1.ejercicio2;
+
+public class Empleado {
+
+    private String nombre, apellido;
+    private int dni;
+    private double salario;
+
+    public Empleado() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
+
+    public double salarioAnual(){
+        this.salario = salario;
+        return salario * 12;
+    }
+
+    public void aumento(int porAumento){
+       double aumento = ((salarioAnual() * porAumento) / 100);
+       double aumentoTotal = aumento + salarioAnual();
+       System.out.println("Empleado- " + nombre + " " + apellido + ". Su salario anual con 15% de aumento es: " + aumentoTotal + ".");
+    }
+
+    public void mostrarEmpleado(){
+        System.out.println("Empleado-  Dni: " + dni + " " + nombre + " " + apellido + " Salario: " + salario );
+    }
+
+
+
+
+
+}
---
+++ GuiaObjetos1/ejercicio2/main.java
@@ -0,0 +1,46 @@
+package GuiaObjetos1.ejercicio2;
+
+public class main {
+
+    /*Inicialice un empleado Carlos Gutiérrez, con dni 23456345 y salario inicial de
+25000.
+    b. Inicialice un empleado Ana Sánchez, con dni 34234123 y salario inicial de
+27500.*/
+    public static void main (String[] args) {
+
+        Empleado emple1 = new Empleado();
+        Empleado emple2 = new Empleado();
+
+        /*Empleado 1, Carlos Gutiérrez, con dni 23456345 y salario inicial de 25000.*/
+        emple1.setNombre("Carlos");
+        emple1.setApellido("Gutierrez");
+        emple1.setDni(23456345);
+        emple1.setSalario(25000);
+
+        /*Empleado 2, Ana Sánchez, con dni 34234123 y salario inicial de 27500.*/
+        emple2.setNombre("Ana");
+        emple2.setApellido("Sánchez");
+        emple2.setDni(34234123);
+        emple2.setSalario(27500);
+
+        emple1.mostrarEmpleado();
+        emple2.mostrarEmpleado();
+
+
+
+        double salarioEmple1 = emple1.salarioAnual();
+        double salarioEmple2  =emple2.salarioAnual();
+
+        System.out.println(emple1.getNombre() + "," + " su salario es de: " + salarioEmple1);
+        System.out.println(emple2.getNombre() + "," + " su salario es de: " + salarioEmple2);
+
+
+        emple1.aumento(15);
+        emple2.aumento(15);
+
+
+
+
+    }
+
+}
